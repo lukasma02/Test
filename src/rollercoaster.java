a31ca832ccafee851c2e9aa0b1259b3231c10c3a
lukasma02 <andersenlukas02@gmail.com>
2025-09-03 11:48:10 +0200

    test

--- src/rollercoaster.java
+++ src/rollercoaster.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.Scanner;
+
+public class rollercoaster {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter name:");
+        String name = sc.nextLine();
+
+        System.out.println("Enter age:");
+        int age = sc.nextInt();
+
+        System.out.println("Enter height:");
+        double height = (int) sc.nextDouble();
+
+        System.out.println("Enter coaster name:");
+        String rollercoastername = sc.nextLine();
+
+        if (height < 190){
+            System.out.println("Congrats you are tall enough" + name +" " + "Enjoy the ride!");
+        } else{
+            System.out.println("You are not tall enough" + " " + name + " " + "Go away");
         }
     }
 }
